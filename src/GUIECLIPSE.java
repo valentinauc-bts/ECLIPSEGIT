5b3a037b97944a98db35441d10e573b972c14ac6
VALENT\Alfonso Buendia <ucvalentina4@gmail.com>
2026-03-26 10:16:32 -0600

    Puse el nombre Valentina

---
+++ src/GUIECLIPSE.java
@@ -0,0 +1,54 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class GUIECLIPSE extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUIECLIPSE frame = new GUIECLIPSE();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GUIECLIPSE() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnValentina = new JButton("Valentina");
+		btnValentina.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(btnValentina, "Hola desde la rama Valentina");
+			}
+			
+		});
+		btnValentina.setBounds(29, 48, 88, 22);
+		contentPane.add(btnValentina);
+
+	}
+}
